4c2cbe31eb6b152911d02c7368e074ea99003c69
Joel Mendez <joel.mendez@gmail.com>
2020-01-23 17:32:21 +1100

    completed lesson 13

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import './global.css';
+
+import BadgeNew from './pages/BadgeNew';
 
 const container = document.getElementById('app');
 
-const element = <h1>Hello, Platzi Badges</h1>;
-ReactDOM.render(element, container);
+ReactDOM.render(<BadgeNew />, container);
